cc3f5bfe946a6b147dca84d93c1cd53af20f16b7
mgrillo <mirco.grillo@assembly.it>
2017-10-04 10:08:00 +0200

    code recursions removed!

--- app/database_methods/project/create_project.py
+++ app/database_methods/project/create_project.py
@@ -1,26 +1,12 @@
 from app.config.utilities import *
 import pymysql as mysql
 
+
 def create_project(title):
-  connection_to_db = connect()
-  res = None
-  msg = ''
-  try:
-      with connection_to_db.cursor() as cursor: #cursor.close() automatico alla fine del try o except...
-          query = "insert into progetto(titolo) values (%s)"
-          try:
-              cursor.execute(query,(title))
-              connection_to_db.commit()
-              msg = 'Created!'
-              print(msg)
-          except (mysql.err.IntegrityError,mysql.err.ProgrammingError) as err:
-              print('Error! Closing connection...' + str(err))
-              error = 'Something went wrong...check your logs...'
-              print(error)
-  finally:
-      if connection_to_db is not None:
-        connection_to_db.close()
-      else:
-          print("Is MySQL Server running ?")
-          return "Is MySQL Server running ?"
-  return msg
+    connection_to_db = connect()
+    query = "insert into progetto(titolo) values (%s)"
+    response = safety_insert_query(connection_to_db, query, title)
+    if response:
+        return response
+    else:
+        return "Something went wrong..."
--- app/database_methods/project/delete_project.py
+++ app/database_methods/project/delete_project.py
@@ -3,24 +3,9 @@ import pymysql as mysql
 
 def delete_project(title):
   connection_to_db = connect()
-  res = None
-  msg = ''
-  try:
-      with connection_to_db.cursor() as cursor: #cursor.close() automatico alla fine del try o except...
-          query = "delete from progetto where titolo=%s"
-          try:
-              cursor.execute(query,(title))
-              connection_to_db.commit()
-              msg = 'Deleted!'
-              print(msg)
-          except (mysql.err.IntegrityError,mysql.err.ProgrammingError) as err:
-              print('Error! Closing connection...' + str(err))
-              error = 'Something went wrong...check your logs...'
-              print(error)
-  finally:
-      if connection_to_db is not None:
-        connection_to_db.close()
-      else:
-          print("Is MySQL Server running ?")
-          return "Is MySQL Server running ?"
-  return msg
+  query = "delete from progetto where titolo=%s"
+  response = safety_delete_query(connection_to_db,query,title)
+  if response:
+      return response
+  else:
+      return "Something went wrong..."
